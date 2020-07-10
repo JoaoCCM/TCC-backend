0ebb663a7adb411c9cc6bb771baf0dc12dc195e5
Beatriz Miranda <beatriz.miranda@acct.global>
2020-07-10 08:09:56 -0300

    removing replaces

--- utils/convert.js
+++ utils/convert.js
@@ -1,21 +1,21 @@
 var convert = require("xml-js");
 var fs = require("fs");
 
-// var xml = fs.readFileSync(__dirname + "/teste01.xml");
+var xml = fs.readFileSync(__dirname + "/teste01.xml");
 
-var xml =
-    '<?xml version="1.0" encoding="UTF-8"?>' +
-    '<note importance="high" logged="true">' +
-    "    <title>Happy</title>" +
-    "    <todo>Work</todo>" +
-    "    <todo>Play &</todo>" +
-    "</note>";
+// var xml =
+//     '<?xml version="1.0" encoding="UTF-8"?>' +
+//     '<note importance="high" logged="true">' +
+//     "    <title>Happy</title>" +
+//     "    <todo>Work</todo>" +
+//     "    <todo>Play &</todo>" +
+//     "</note>";
 
 var result2 = convert.xml2json(
-    xml
-        .replace(/[\n\r]/g, "\\n")
-        .replace(/&/g, "&amp;")
-        .replace(/-/g, "&#45;"),
+    xml,
+    // .replace(/[\n\r]/g, "\\n")
+    // .replace(/&/g, "&amp;")
+    // .replace(/-/g, "&#45;"),
     {
         compact: false,
         spaces: 4,
