80fd554a56a2b498734ef4c28ab949db0ff6c6f2
Beatriz Miranda <beatriz.miranda@acct.global>
2020-10-11 14:09:08 -0300

    xml from scriptlattes and function to convert and filter

--- utils/convert.js
+++ utils/convert.js
@@ -1,52 +1,23 @@
 var convert = require("xml-js");
 const fs = require("fs");
 
-var xml = fs.readFileSync(__dirname + "/teste01.xml");
-
-var result2 = convert.xml2json(
-    xml,
-    // .replace(/[\n\r]/g, "\\n")
-    // .replace(/&/g, "&amp;")
-    // .replace(/-/g, "&#45;"),
-    {
-        compact: true,
-        spaces: 4,
+var xml = fs.readFileSync(
+    __dirname + "/professores.xml",
+    "utf-8",
+    (err, data) => {
+        if (err) console.error(err);
+        return data.replace(/&/g, "&amp;");
     }
 );
 
-//se o arquivo não existir o filesystem cria
-fs.writeFileSync("./teste01.json", result2, (err) => {
-    console.error(err);
+var xmlConvertedToJSON = convert.xml2json(xml, {
+    compact: true,
+    spaces: 4,
+    ignoreAttributes: true,
+    // attributeValueFn: (attrValue) => entities.encodeXML(attrValue),
 });
 
-const filterJSON = (data, id) => {
-    const filteredFields = ["identificacao", "idiomas"];
-    const keys = Object.keys(data);
-
-    // const filterInternValues = {
-    //     identificacao: ["nome_completo"],
-    // };
-
-    const filteredJson = keys
-        .filter((key) => filteredFields.includes(key))
-        .reduce((obj, key) => {
-            obj[key] = data[key];
-            return obj;
-        }, {});
-
-    return { lattesId: id, ...filteredJson };
-};
-
-var json = require("./teste01.json");
-
-const filteredJson = {};
-
-filteredJson.items = json.curriculo_lattes.pesquisador.map((it) =>
-    filterJSON(it, it._attributes.id)
-);
-
-console.log(filteredJson);
-
-fs.writeFileSync("./filteredJSON.json", JSON.stringify(filteredJson), (err) => {
+//se o arquivo não existir o filesystem cria
+fs.writeFileSync("./professores.json", xmlConvertedToJSON, (err) => {
     console.error(err);
 });
